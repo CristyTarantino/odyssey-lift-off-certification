5e5b116cae9e030e1ac4a406cc5d949dba3d8b6a
Cristina Tarantino <cristina@tarantino.io>
2022-03-30 11:34:25 +0100

    add track route and query in client and server

---
+++ server/src/resolver.js
@@ -0,0 +1,30 @@
+const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate
+    // the homepage grid of our web client
+    tracksForHome: (_, __, {dataSources}) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+
+    // get a single track by id, for the Track page
+    track: (_, {id}, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id)
+    }
+  },
+  /*
+   The parent argument contains data returned by our tracksForHome resolver,
+   and because tracksForHome returns a list,
+   Apollo Server iterates through that list and calls the author resolver once for each track.
+   It passes the current track as the value of parent, enabling us to extract the authorId.
+   */
+  Track: {
+    author: ({authorId}, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id)
+    }
+  },
+}
+
+module.exports = resolvers;
